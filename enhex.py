3d97d98c60bae1c38a4b8ca1bd505152beaf2aa0
noisecrust <32306174+NoiseCrusT@users.noreply.github.com>
2018-06-06 00:19:58 +0700

    This Converter
    Convert Text To Hex

---
+++ enhex.py
@@ -0,0 +1,12 @@
+print """
+TEXT To HEX Encode
+
+Coded By Afrizal F.A
+"""
+a = raw_input ('Masukkan Text = ').rstrip()
+def hexcape(stri):
+   f = ""
+   for x in stri:
+      f += "\\x"+x.encode('hex')
+   return f
+print(hexcape(a))
